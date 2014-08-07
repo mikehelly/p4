4ab0c3883f275ea8c551fbad544bee4cc16fa898
Mike Helly <mike@mikehelly.com>
2014-08-07 07:45:04 -0400

    environment settings

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => '127.3.241.2',
+            'database'  => 'p4',
+            'username'  => 'adminzELlqGD',
+            'password'  => 'WJspQfL4Heci',
+        ),
+
+    ),
+
+);
